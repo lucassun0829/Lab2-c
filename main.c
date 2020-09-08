f2fc22a056a99e59582156e4bb769d801e02ef3e
lucassun0829 <lks5713@psu.edu>
2020-09-08 15:29:35 +0000

    change C to C+

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(void) {
     printf("Your letter grade for CMPSC 131 is B-.\n");
   }
   else if(num>=77.0){
-    printf("Your letter grade for CMPSC 131 is C-.\n");
+    printf("Your letter grade for CMPSC 131 is C+.\n");
   }
   else if(num>=70.0){
     printf("Your letter grade for CMPSC 131 is C.\n");
